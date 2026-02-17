008941bc311d64a152a28770c3e9abd57fcbfddb
shre-coder638 <shankarshreyash79@gmail.com>
2026-02-17 20:34:04 +0530

    Initialize FSR analog pin

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,9 @@
  * displays structured output via Serial Monitor.
  */
 
- // TODO 1:
- // Define FSR analog pin (Use A0)
+#define FSR_PIN A0
 
- // TODO 2:
- // Create variable to store sensor reading
+int fsrValue = 0;
 
 void setup() {
 
