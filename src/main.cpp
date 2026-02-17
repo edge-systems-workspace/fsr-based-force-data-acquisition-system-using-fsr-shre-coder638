ae15105e657f350777ef6816ff6800ccc588d3c7
shre-coder638 <shankarshreyash79@gmail.com>
2026-02-17 20:35:09 +0530

    Initialize Serial communication at 9600 baud rate

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,9 @@ int fsrValue = 0;
 
 void setup() {
 
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
 
-    // TODO 4:
-    // Print system initialization message
+    Serial.println("=== FSR Force Measurement System Initialized ===");
 }
 
 void loop() {
