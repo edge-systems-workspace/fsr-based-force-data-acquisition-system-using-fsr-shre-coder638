a3e85f3104411b686509b7469b130228cfc7b6a7
shre-coder638 <shankarshreyash79@gmail.com>
2026-02-17 20:36:09 +0530

    Reading analog value from FSR

--- src/main.cpp
+++ src/main.cpp
@@ -15,19 +15,16 @@
 int fsrValue = 0;
 
 void setup() {
-
     Serial.begin(9600);
-
     Serial.println("=== FSR Force Measurement System Initialized ===");
 }
 
 void loop() {
 
-    // TODO 5:
-    // Read analog value from FSR
+    fsrValue = analogRead(FSR_PIN);
 
-    // TODO 6:
-    // Print raw ADC value
+    Serial.print("Raw ADC Value: ");
+    Serial.println(fsrValue);
 
     // TODO 7:
     // Apply simple threshold logic (e.g., detect pressure)
